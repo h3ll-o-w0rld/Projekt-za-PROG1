50ecd994d536acf4b5fc5ff55f1209cd98f6a9e6
Jasmina Pegan <jasmina.pegan@student.fmf.uni-lj.si>
2016-10-17 11:50:50 +0200

    osveženo

--- tabele.py
+++ tabele.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import pandas as pd
-
+import csv
 
 datoteka = 'skupine.csv'
 celotna_tabela = pd.read_csv(datoteka, sep = ',', index_col = 'ime')
@@ -8,5 +8,16 @@ celotna_tabela.str.encode('utf-8')
 zvrsti = celotna_tabela['zvrst']
 statusi = celotna_tabela['status']
 drzave = celotna_tabela['drz']
-osnovna_tabela = celotna_tabela[['ime', 'leto']]
-print(osnovna_tabela)
+osnovna_tabela = celotna_tabela['leto']
+
+osn_slovarji = [
+    {'ime': line['ime'], 'leto': line['leto']} for line in osnovna_tabela
+    ]
+
+with open('osnovno.csv', 'w') as csv_dat:
+    writer = csv.DictWriter(csv_dat, fieldnames=['ime', 'leto'])
+    writer.writeheader()
+    for slovar in osnovna_tabela:
+        writer.writerow(slovar)
+
+
